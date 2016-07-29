77f32117eb2f2b7457f2d35c0c9e600d1106f37d
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2016-07-29 18:50:02 +0300

    Change because of duplication issue

--- config/di/aliases.inc.php
+++ config/di/aliases.inc.php
@@ -7,7 +7,6 @@ return [
     'DataFilterListener' => DI\object(Sarcofag\SPI\EventManager\GenericListener::class),
     'DefaultStaticPostController' => DI\object(Sarcofag\Theme\Controller\StaticPostController::class),
     'DefaultStaticPageController' => DI\object(Sarcofag\Theme\Controller\StaticPostController::class),
-    'Renderer' => DI\object(Sarcofag\View\Renderer\SimpleRenderer::class),
     'GenericMenu' => DI\object(Sarcofag\SPI\Menu\Menu::class),
     'GenericSidebar' => DI\object(Sarcofag\SPI\Sidebar\SidebarEntry::class),
     'MenuRegistry' => DI\object(Sarcofag\SPI\Menu\Registry::class),
