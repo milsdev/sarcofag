ada72c218dc0bad39999ec2bab0e0b2f0f37191f
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2016-09-18 15:59:40 +0300

    Specify code style rules, changed LICENSE and fix a code style for the SimpleRenderer

--- src/View/Renderer/SimpleRenderer.php
+++ src/View/Renderer/SimpleRenderer.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Sarcofag (http://sarcofag.com)
+ *
+ * @link       https://github.com/milsdev/sarcofag
+ * @copyright  Copyright (c) 20012-2016 Mil's (http://www.mils.agency)
+ * @license    http://sarcofag.com/license/mit
+ */
+
 namespace Sarcofag\View\Renderer;
 
 use Sarcofag\API\WP;
@@ -6,8 +14,16 @@ use Sarcofag\Exception\RuntimeException;
 use Psr\Http\Message\ResponseInterface;
 use Sarcofag\View\Helper\HelperManagerInterface;
 
+/**
+ * Simple Renderer.
+ *
+ * This renderer responsible for the rendering phtml
+ * views, in context with Helpers and local scope,
+ * to avoid global scope conflicts.
+ */
 class SimpleRenderer implements RendererInterface, PsrHttpRendererInterface
 {
+
     /**
      * @var HelperManagerInterface
      */
@@ -23,35 +39,41 @@ class SimpleRenderer implements RendererInterface, PsrHttpRendererInterface
      */
     protected $wp;
 
+
     /**
      * SimpleRenderer constructor.
      *
-     * @param HelperManager $helperManager
-     * @param array $templaterConfig
-     * @param WP $wp
+     * @param HelperManagerInterface $helperManager   Container with all Helpers.
+     * @param array                  $templaterConfig Array with the pathes to the template/view dirs.
+     * @param WP                     $wp              Wordpress API object, to be able to call wordpress specific
+     *                                                functions.
      */
-    public function __construct(HelperManagerInterface $helperManager,
-                                array $templaterConfig,
-                                WP $wp)
-    {
+    public function __construct(
+        HelperManagerInterface $helperManager,
+        array $templaterConfig,
+        WP $wp
+    ) {
         $this->templateConfig = $templaterConfig;
-        $this->helperManager = $helperManager;
-        $this->wp = $wp;
+        $this->helperManager  = $helperManager;
+        $this->wp             = $wp;
     }
 
+
     /**
-     * @param $template
+     * @param string $template Relative path to the template.
      *
      * @return string
+     * @throws RuntimeException Throws if template has incorrect format or template could not be found.
      */
     protected function getTemplatePath($template)
     {
         $parts = explode('/', $template);
         if (count($parts) < 1) {
-            throw new RuntimeException('Template path ['.$template.'] has incorrect format, '.
-                                        'it is must start from theme-name or alias path');
+            throw new RuntimeException(
+                "Template path [{$template}] has incorrect format, it is must start from theme-name or alias path"
+            );
         }
-        
+
         $extracted = array_splice($parts, 0, 1);
 
         $alias = $extracted[0];
@@ -59,29 +81,17 @@ class SimpleRenderer implements RendererInterface, PsrHttpRendererInterface
             $alias = $this->wp->wp_get_theme()->template;
             array_unshift($parts, $extracted[0]);
         }
-        
 
-        $fullTemplatePath = rtrim($this->templateConfig[$alias], '/') . '/' . join('/', $parts);
-        
+
+        $fullTemplatePath = rtrim($this->templateConfig[$alias], '/').'/'.join('/', $parts);
+
         if (!file_exists($fullTemplatePath)) {
-            throw new RuntimeException('Could not found template ['.$fullTemplatePath.'] '.
-                                            'please check requested template name');
+            throw new RuntimeException(
+                "Could not found template [{$fullTemplatePath}] please check requested template name"
+            );
         }
-        
+
         return $fullTemplatePath;
-    }
-
-    /**
-     * @param string $templatePath
-     * @param string $alias [OPTIONAL]
-     */
-    public function setThemeTemplatePath($templatePath, $alias = '')
-    {
-        if (empty($alias)) {
-            $alias = $this->wp->wp_get_theme()->template;
-        }
-
-        $this->templateConfig[$alias] = $templatePath;
     }
 
     /**
@@ -91,41 +101,42 @@ class SimpleRenderer implements RendererInterface, PsrHttpRendererInterface
      *
      * throws RuntimeException if $templatePath . $template does not exist
      *
-     * @param                    $template
-     * @param array              $data
+     * @param string $template Relative path to the template to render in current context.
+     * @param array  $data     Variables and different data which must be included into rendering context.
      *
      * @return string
-     *
-     * @throws \InvalidArgumentException
-     * @throws \RuntimeException
+     * @throws RuntimeException Throws if template has incorrect format or template could not be found.
      */
     public function render($template, array $data = [])
     {
-        $render = \Closure::bind(function ($template, $data) {
-            extract($data);
-            include $template;
-        }, $this->helperManager, get_class($this->helperManager));
+        $render = \Closure::bind(
+            function ($template, $data) {
+                extract($data);
+                include $template;
+            },
+            $this->helperManager,
+            get_class($this->helperManager)
+        );
 
         ob_start();
         $render($this->getTemplatePath($template), $data);
         return ob_get_clean();
     }
 
+
     /**
      * Response with template
      *
      * $data cannot contain template as a key
      *
-     * throws RuntimeException if $templatePath . $template does not exist
+     * throws RuntimeException if $templatePath.$template does not exist
      *
-     * @param ResponseInterface $response
-     * @param                    $template
-     * @param array              $data
+     * @param ResponseInterface $response Response object to contain rendered template output.
+     * @param string            $template Template path to the requesting template.
+     * @param array             $data     Variables and different data to include into the rendering context.
      *
      * @return ResponseInterface
-     *
-     * @throws \InvalidArgumentException
-     * @throws \RuntimeException
+     * @throws RuntimeException Throws if template has incorrect format or template could not be found.
      */
     public function response(ResponseInterface $response, $template, array $data = [])
     {
