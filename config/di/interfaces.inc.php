7f5f3361330957be27928773471570003379218b
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2016-11-14 21:30:00 +0200

    Remove forgoten pieces of configuration, which not in use anymore.

--- config/di/interfaces.inc.php
+++ config/di/interfaces.inc.php
@@ -9,12 +9,6 @@ return [
     \Sarcofag\View\Helper\HelperManagerInterface::class =>
         DI\get('HelperManager'),
 
-    \Sarcofag\SPI\RegisterActionInterface::class =>
-        DI\get('EventsRegistrator'),
-
-    \Sarcofag\SPI\RegisterFilterInterface::class =>
-        DI\get('EventsRegistrator'),
-
     \Sarcofag\SPI\EventManager\EventManagerInterface::class =>
         DI\get('EventManager')
 ];
